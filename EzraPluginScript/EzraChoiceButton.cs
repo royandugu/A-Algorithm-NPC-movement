85f20d4568d473398df9e1c34867ded40f0f484c
royandugu <royandugu99009@gmail.com>
2024-10-29 21:38:21 +0545

    Instantiation of button scene

--- EzraPluginScript/EzraChoiceButton.cs
+++ EzraPluginScript/EzraChoiceButton.cs
@@ -1,11 +1,21 @@
 using Godot;
 public partial class EzraChoiceButton:Button{
+    private int choiceIndex=0;
+    public int ChoiceIndex
+    {
+        get => choiceIndex;
+        set => choiceIndex = value;
+    }
     public override void _Ready()
     {
-    
+        this.Pressed+=OnPressed;
     }
 
     public override void _Process(double delta){
         
+    }
+
+    public void OnPressed(){
+        GD.Print("button is pressed");
     }
 }
--- EzraPluginScript/EzraDialog.cs
+++ EzraPluginScript/EzraDialog.cs
@@ -1,9 +1,25 @@
+using System.Collections.Generic;
 using Godot;
 
 public partial class EzraDialog:Node2D{
     [Export]
     private Label textBox; 
+    PackedScene buttonScene = (PackedScene)ResourceLoader.Load("res://Scenes/ChoiceButton.tscn");
+    private List<Button> buttons;
+    private VBoxContainer vBoxContainer;
+
+    public override void _Ready()
+    {
+        vBoxContainer = GetNode<VBoxContainer>("VBoxContainer");
+    }
     public void AddText(string text){
         textBox.Text = text;
     }   
+
+    public void AddChoice(){
+        EzraChoiceButton buttonObj = (EzraChoiceButton) buttonScene.Instantiate();
+        buttonObj.ChoiceIndex=buttons.Count;
+        buttons.Add(buttonObj);
+        vBoxContainer.AddChild(buttonObj);
+    }
 }
